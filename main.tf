61c9b1baf79c6a352cad72c8f7617bcd39482da9
Devin Lauderdale <devinlauderdale2@gmail.com>
2024-07-16 06:38:38 -0500

    refactor: move outputs to own file (remove)

--- main.tf
+++ main.tf
@@ -113,17 +113,3 @@ resource "aws_security_group" "this" {
 data "aws_vpc" "database_vpc" {
   id = var.vpc_id
 }
-
-output "db_cluster_id" {
-  value = aws_rds_cluster.this.cluster_identifier
-}
-
-output "security_group_id" {
-  value = aws_security_group.this.id
-}
-output "root_password_secret_id" {
-  value = aws_secretsmanager_secret.root_password.id
-}
-output "connection_string_arn" {
-  value = aws_secretsmanager_secret.connection_string.arn
-}
